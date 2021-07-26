871065c79bfddc6ad51d493a0315cbbdd4d23af7
BuG-Dev-Joris <maupied69@hotmail.com>
2021-07-26 13:36:18 +0200

    fixed title and css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 
 import logo from "../../src/logo.svg";
 
 export default function Navbar() {
-  return (
-      <div className="navbar">
-      <img alt="profilepic" src={logo} className="App-logo"></img>
-        <h1>Portofolio de Joris Maupied</h1>
-      <a href="/home">Projects</a>
-      <a href="/Contact">Contact</a>
-      <button type="button">Toggle</button>
-      </div>
-  );
+	return (
+		<>
+			<div className="navbar">
+				<img alt="profilepic" src={logo} className="App-logo"></img>
+				<div className="title-links">
+					<div className="title">
+						<h1>Portfolio de Joris Maupied</h1>
+					</div>
+					<div className="links">
+						<a href="/home">Projects</a>
+						<a href="/Contact">Contact</a>
+						<button className="mr-0" type="button">Toggle</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
